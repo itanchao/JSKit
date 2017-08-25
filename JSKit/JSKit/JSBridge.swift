31ff0276fad48c7a980e2bcd52d556d158db217e
谈超 <itanchao@gmail.com>
2017-08-26 00:36:34 +0800

    删除代码

--- JSKit/JSKit/JSBridge.swift
+++ JSKit/JSKit/JSBridge.swift
@@ -38,9 +38,9 @@ extension JSBridge : WKScriptMessageHandler{
                 }
                 DispatchQueue.main.async {
                     self.webView?.evaluateJavaScript(jscode, completionHandler: { (data, error) in
-                        JSLogger.debug("methodName:\(dic["methodName"] as? String ?? "")\nparams:\(dic["params"] as? [String:Any] ?? [String:Any]())\ncallBackName:\(callBackName),JS=\(jscode)")
+                        print("methodName:\(dic["methodName"] as? String ?? "")\nparams:\(dic["params"] as? [String:Any] ?? [String:Any]())\ncallBackName:\(callBackName),JS=\(jscode)")
                         if (error != nil){
-                            JSLogger.debug("evaluateJavaScript:error  \(String(describing: error))")
+                            print("evaluateJavaScript:error  \(String(describing: error))")
                         }
                     })
                 }
@@ -49,7 +49,7 @@ extension JSBridge : WKScriptMessageHandler{
     }
     func jsAction(methodName : String?, params:[String:Any] = [String:Any](),_ callback:@escaping ((_ response:Any?,_ resultType:Bool)->()) ) {
         guard methodName != nil else {
-            JSLogger.debug("methodName为空")
+            print("methodName为空")
             return
         }
         let diction = NSMutableDictionary(dictionary: params as NSDictionary)
@@ -57,110 +57,11 @@ extension JSBridge : WKScriptMessageHandler{
             if responds(to: Selector("\(methodName ?? ""):")){
                 self.perform(Selector("\(methodName ?? ""):"), with: diction )
         }
-    
     }
     @objc func openBluetoothAdapter(_ params:[String:Any]) {
-        JSLogger.debug(params)
+        print(params)
         if let callbackMethod =  params["callbackMethod"] as? ((_ response:Any?,_ resultType:Bool)->()){
             callbackMethod(["isSupportBLE":true],true)
         }
-    }
-}
-
-fileprivate class JSLogger {
-    
-    public static let FATAL_LEVEL = 1000
-    public static let ERROR_LEVEL = 900
-    public static let WARN_LEVEL = 800
-    public static let INFO_LEVEL = 700
-    public static let DEBUG_LEVEL = 600
-    public static let TRACE_LEVEL = 500
-    
-    public static var LOG_LEVEL = TRACE_LEVEL
-    
-    public static func debug(
-        _ message: Any = "",
-        file: String = #file,
-        line: UInt = #line,
-        function: String = #function) {
-        if LOG_LEVEL <= DEBUG_LEVEL {
-            log("DEBUG", message: message, file: file, line: line, function: function, verbose: true)
-        }
-    }
-    
-    public static func fatal(
-        _ message: Any = "",
-        file: String = #file,
-        line: UInt = #line,
-        function: String = #function) {
-        
-        if LOG_LEVEL <= FATAL_LEVEL {
-            log("FATAL", message: message ,file: file, line: line, function: function, verbose: true)
-        }
-        
-    }
-    
-    public static func error(
-        _ message: Any = "",
-        file: String = #file,
-        line: UInt = #line,
-        function: String = #function) {
-        if LOG_LEVEL <= ERROR_LEVEL {
-            log("ERROR", message: message, file: file, line: line, function: function, verbose: true)
-        }
-    }
-    
-    public static func warn(
-        _ message: Any = "",
-        file: String = #file,
-        line: UInt = #line,
-        function: String = #function) {
-        if LOG_LEVEL <= WARN_LEVEL {
-            log("WARN", message: message, file: file, line: line, function: function, verbose: true)
-        }
-    }
-    
-    public static func info(
-        _ message: Any = "",
-        file: String = #file,
-        line: UInt = #line,
-        function: String = #function) {
-        if LOG_LEVEL <= INFO_LEVEL {
-            log("INFO", message: message, file: file, line: line, function: function, verbose: true)
-        }
-    }
-    
-    public static func trace(
-        _ message: Any = "",
-        file: String = #file,
-        line: UInt = #line,
-        function: String = #function) {
-        if LOG_LEVEL <= TRACE_LEVEL {
-            log("TRACE", message: message, file: file, line: line, function: function, verbose: true)
-        }
-    }
-    
-    public static func log (
-        _ prefix: String,
-        message: Any = "",
-        file: String = #file,
-        line: UInt = #line,
-        function: String = #function,
-        verbose: Bool) {
-        
-        let aDateFormat = DateFormatter()
-        aDateFormat.dateFormat = "YYYY-MM-dd hh:mm:ss"
-        let time = aDateFormat.string(from: Date())
-        
-        var fileName = "Unknown File"
-        if let theFileName = file.components(separatedBy: "/").last {
-            fileName = theFileName
-        }
-        if verbose {
-            print("[\(prefix)] \(time) IN \(fileName) LINE: \(line) FUNC: \(function) - \(message)")
-        }else{
-            print("[\(prefix)] \(time) \(message)")
-        }
-        
     }
 }
